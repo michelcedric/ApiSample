ff3ed4b0b6fb7f3009aeb1c3e6355273cae56482
Cedric <cedric.michel@outlook.be>
2020-02-18 21:23:26 +0100

    add test project structure

---
+++ ShowApiCleanCode/tests/Unit tests/ApplicationCore.UnitTests/Services/WeatherForecastServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.UnitTests.Services
+{
+    [TestClass]
+    public class WeatherForecastServiceTest
+    {
+        [TestMethod]
+        public async Task GetAllAsync_Test()
+        {
+            await Task.CompletedTask;
+            Assert.IsTrue(true);
+        }
+    }
+}
